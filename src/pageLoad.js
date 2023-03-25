2cb62ddd5483a059b2c31dc83f835891cfca49b3
HyunCafe <eparksuncafe@gmail.com>
2023-03-25 14:47:31 -0500

    Change source file paths for images again

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -105,7 +105,7 @@ export const createMain = () => {
     staggeredItem.classList.add("staggered-item");
 
     const image = document.createElement("img");
-    image.src = `../src/assets/bg${i}.jpg`;
+    image.src = `./assets/bg${i}.jpg`;
     image.alt = "Restaurant Name";
     image.classList.add("staggered-image");
 
